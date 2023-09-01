3bdc201ff157f3742d45fbdb3a80bf58bef4f055
md-mehadihassan <mdmehadihassan523@gmail.com>
2023-09-01 10:49:37 +0600

    Triangle

---
+++ Triangle/triangle.php
@@ -0,0 +1,9 @@
+<?php
+$h= 10;//h=height.
+    $b = 8;//b=base.
+    $triangle = 0.5 * $h * $b;
+    echo "Area of the triangle : ".$triangle;
+    echo "<br>";
+
+    //$b = 2 * $triangle / $height;
+    //echo "Base of the triangle : ".$b;
